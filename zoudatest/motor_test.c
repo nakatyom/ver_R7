74f2db7f0f94dc25ae7df37a401aba2254e8f6a8
NomuraNaoki <nomk8755@icloud.com>
2024-07-17 19:59:14 +0900

    For test motor API

---
+++ zoudatest/motor_test.c
@@ -0,0 +1,17 @@
+#ifndef motor_test_H
+#define motor_test_H
+
+#include "ev3api.h"
+#include "etroboc_ext.h"
+#include <stdio.h>
+
+//関数プロトタイプ宣言
+static void motorLog();
+
+/* ステアリング操舵量の計算 */
+static void motorTypeLog(){
+    printf("left_motor's port is %d, and it's power is %d \n",ev3_motor_get_type(left_motor),ev3_motor_get_power(left_motor));
+    printf("right_motor's port is %d, and it's power is %d \n",ev3_motor_get_type(right_motor),ev3_motor_get_type(right_motor););
+}
+
+#endif
