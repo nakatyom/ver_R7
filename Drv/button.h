193e34e4239590b04be1c6712c7e30c19583c392
Ichic <Ichicoma@outlook.jp>
2024-07-21 22:47:27 +0900

    add battery and button driver programs.

---
+++ Drv/button.h
@@ -0,0 +1,12 @@
+#ifndef _BUTTON_H
+#define _BUTTON_H
+
+#include "ev3api.h"
+#include "app.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+/* external functions */
+bool get_buttonState(button_t button);
+
+#endif "_BUTTON_H"
