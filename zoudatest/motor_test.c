4234c67ab0dfd6852b0f1cd4114a0f9ab48cfe2a
NomuraNaoki <nomk8755@icloud.com>
2024-07-22 19:59:37 +0900

    Delete NAKA_TASK

--- zoudatest/motor_test.c
+++ zoudatest/motor_test.c
@@ -14,7 +14,7 @@ static void motorLog();
 /* ステアリング操舵量の計算 */
 static void motorTypeLog(){
     printf("left_motor's port is %d, and it's power is %d \n",ev3_motor_get_type(left_motor),ev3_motor_get_power(left_motor));
-    printf("right_motor's port is %d, and it's power is %d \n",ev3_motor_get_type(right_motor),ev3_motor_get_type(right_motor););
+    printf("right_motor's port is %d, and it's power is %d \n",ev3_motor_get_type(right_motor),ev3_motor_get_type(right_motor));
 }
 
 #endif
