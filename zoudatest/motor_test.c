26aa4dc898cc4ac04fdef18a61fc3eb3b221d0d2
masuda-ie <h.masuda.ie@hitachi-kenki.com>
2024-07-23 10:16:00 +0900

    ev3_motor_set_power API used

--- zoudatest/motor_test.c
+++ zoudatest/motor_test.c
@@ -7,12 +7,36 @@
 
 //関数プロトタイプ宣言
 extern void motorLog();
+extern void goetrobo();
 
-
-
-/* ステアリング操舵量の計算 */
+/* モーター情報表示 */
 void motorLog(){
-    printf("left_motor's port is %d, and it's power is %d \n",ev3_motor_get_type(left_motor),ev3_motor_get_power(left_motor));
-    printf("right_motor's port is %d, and it's power is %d \n",ev3_motor_get_type(right_motor),ev3_motor_get_type(right_motor));
+    
+    int left_power, right_power;
+    
+    left_power = ev3_motor_get_power(left_motor);
+    right_power = ev3_motor_get_type(right_motor);
+    
+    printf("left_motor's port is %d, and it's power is %d \n",ev3_motor_get_type(left_motor),left_power);
+    printf("right_motor's port is %d, and it's power is %d \n",ev3_motor_get_type(right_motor),right_power);
 }
 
+//スピード・向きを変えながら進む
+void goetrobo(){
+    int i;
+    //モーターの出力設定
+    int set_lpower, set_rpower;
+    switch(i){
+        case 1 : set_lpower = 10; set_rpower = 10; i=1; break;
+        case 2 : set_lpower = 5; set_rpower = 10; i++; break;
+        case 3 : set_lpower = 0; i++; set_rpower = 10; break;
+        case 4 : set_lpower = -10; set_rpower = 10; i++; break;
+        case 5 : set_lpower = 0; set_rpower = 0; i++; break;
+        case 6 : set_lpower = 10; set_rpower = 5; i++; break;
+        case 7 : set_lpower = 20; set_rpower = 10; i++; break;
+    }
+
+    //モーター出力
+    ev3_motor_set_power(left_motor, set_lpower);
+    ev3_motor_set_power(right_motor, set_rpower);
+}
