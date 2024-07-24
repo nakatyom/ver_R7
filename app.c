bcb56d7be96cc2c99fb5754cc2081cad1aa6ba3c
nakatyom <k.nakano.wz@hotachi-kenki.com>
2024-07-24 09:35:36 +0900

    ジャイロ確認用モデルのコミット。

--- app.c
+++ app.c
@@ -1,32 +1,54 @@
 #include "app.h"
-#include "file_io.c"
 #include <stdio.h>
+#include "ev3api.h"
+#include "color_sensor.h"
+#include "port_settings.h"
+#include "ultrasonic_sensor.h"
+#include "gyro_sensor.h"
+
 
 /* メインタスク(起動時にのみ関数コールされる) */
 void main_task(intptr_t unused) {
 
-    /* センサー入力ポートの設定 */
-    ev3_sensor_config(touch_sensor ,TOUCH_SENSOR);
-    ev3_sensor_config(color_sensor ,COLOR_SENSOR);
-    ev3_sensor_config(sonar_sensor ,ULTRASONIC_SENSOR);
-    ev3_sensor_config(gyro_sensor  ,GYRO_SENSOR);
-    
-    /* モーター出力ポートの設定 */
-    ev3_motor_config(arm_motor     ,LARGE_MOTOR);
-    ev3_motor_config(left_motor    ,MEDIUM_MOTOR);
-    ev3_motor_config(right_motor   ,MEDIUM_MOTOR);
     
     printf("Start MainTask!!\n");
     
     /* ライントレースタスクの起動 */
-    write_log();
+    /* write_log(); */
+    commn_settings();    
+    
+    printf("Start Naka Task!!\n");
     sta_cyc(NAKA_TASK_CYC);
 
     /* タスク終了 */
     ext_tsk();
 }
 
+
+//uint8_t cnt;
 void naka_task(intptr_t unused){
-    printf("Start Naka Task!!\n");
+    /*    
+    cnt = cnt + 1; 
+    if (cnt >= 250) {
+        cnt = 0;
+        drv_gyro_reset();
+    }
+    printf("Main counter:%d\n",cnt);*/
+
+    /* 左右モータ駆動パワーの設定 */
+    //ev3_motor_set_power(left_motor, -90);
+    //ev3_motor_set_power(right_motor, 90);
+
+    // カラーセンサのテスト関数
+    //drv_color_sensor();
     
+    // 共通センサのget関数
+    //get_commn_sensor_settings();
+    
+    // 超音波センサのテスト関数
+    //drv_get_ultrasonic();
+
+    // ジャイロセンサのテスト関数
+    drv_gyro_sensor();
+
 }
--- app.h
+++ app.h
@@ -1,8 +1,12 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 #include "ev3api.h"
+#include "color_sensor.h"
+#include "port_settings.h"
+#include "ultrasonic_sensor.h"
+#include "gyro_sensor.h"
+#include <stdio.h>
 
 /* タスク優先度 */
 #define MAIN_PRIORITY    (TMIN_APP_TPRI + 1) /* メインタスク */
@@ -12,19 +16,6 @@ extern "C" {
 /* タスク周期の定義 */
 #define LINE_TRACER_PERIOD  (100 * 1000) /* ライントレースタスク:100msec周期 */
 
-/* センサーポートの定義 */
-static const sensor_port_t
-    touch_sensor    = EV3_PORT_1,
-    color_sensor    = EV3_PORT_2,
-    sonar_sensor    = EV3_PORT_3,
-    gyro_sensor     = EV3_PORT_4;
-
-/* モーターポートの定義 */
-static const motor_port_t
-    arm_motor       = EV3_PORT_A,
-    left_motor      = EV3_PORT_C,
-    right_motor     = EV3_PORT_B;
-
 #ifndef STACK_SIZE
 #define STACK_SIZE      (4096)
 #endif /* STACK_SIZE */
@@ -33,6 +24,7 @@ static const motor_port_t
 
 extern void main_task(intptr_t exinf);
 extern void naka_task(intptr_t exinf);
+
 
 #endif /* TOPPERS_MACRO_ONLY */
 
