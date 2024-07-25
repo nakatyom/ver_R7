bb40887506c0866c2a1533b3a2c69e5f57a5ba66
Ichic <Ichicoma@outlook.jp>
2024-07-26 00:43:19 +0900

    battery, button functions

---
+++ Drv/battery.c
@@ -0,0 +1,42 @@
+/* Note */
+// These APIs update battery infomations 1[time/second].
+
+#include "ev3api.h"
+#include <stdio.h>
+
+#include "battery.h"
+
+
+/* define */
+#define V_MAX 5000
+#define V_MIN 0
+#define C_MAX 1000
+#define C_MIN 0
+
+/* external functions */
+
+int get_batV(void){
+    int volt = 0;
+
+    volt = ev3_battery_voltage_mV();
+
+    if(volt > V_MAX && volt < V_MIN){
+        printf("voltage value is out of range.\n");
+        // return -1;
+    }
+
+    return volt;
+}
+
+int get_batC(void){
+    int current = 0;
+
+    current = ev3_battery_current_mA();
+
+    if(current > C_MAX && current < C_MIN){
+        printf("current value is out of range.\n");
+        // return -1;
+    }
+
+    return current;
+}
---
+++ Drv/battery.h
@@ -0,0 +1,12 @@
+#ifndef _BATTERY_H
+#define _BATTERY_H
+
+#include "ev3api.h"
+#include "app.h"
+#include <stdio.h>
+
+/* external functions */
+extern int get_batV(void);
+extern int get_batC(void);
+
+#endif /* _BATTERY_H */
