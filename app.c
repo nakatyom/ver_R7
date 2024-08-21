789eadbde48e9c4508f6a091ef7b53bb98bdb9a6
nakatyom <k.nakano.wz@hotachi-kenki.com>
2024-08-21 19:13:51 +0900

    Caribration

--- app.c
+++ app.c
@@ -22,10 +22,28 @@ void main_task(intptr_t unused) {
 }
 
 
-//uint8_t cnt;
+uint8_t ma_val;
+uint8_t mv_val;
+int power_left, power_right;
+int32_t ang_left,ang_right;
 void naka_task(intptr_t unused){
     //printf("10ms Task");
-    mid_linetrace_pid();
+    //mid_linetrace_pid();
+    ang_left = ev3_motor_get_counts(left_motor);
+    ang_right = ev3_motor_get_counts(right_motor);
 
+    ev3_motor_reset_counts(left_motor);
+    ev3_motor_reset_counts(right_motor);
+
+    ma_val = ev3_battery_current_mA();
+    mv_val = ev3_battery_voltage_mV();
+
+    power_left=100;
+    power_right=100;
+    
+    ev3_motor_set_power(left_motor, power_left);
+    ev3_motor_set_power(right_motor, power_right);
+
+    printf("Battery(A,V):(%d,%d) | Power(L,R):(%d,%d) | Angle(L,R)=(%d,%d)\n",ma_val,mv_val,power_left,power_right,ang_left,ang_right);
 
 }
