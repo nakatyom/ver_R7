0adc9ed6f864f555673851d9677b210b93608736
市毛大貴 <d.ichige.cb@hitachi-kenki.com>
2024-08-22 19:22:26 +0900

    test motor

--- app.c
+++ app.c
@@ -7,23 +7,28 @@
 
 /* メインタスク(起動時にのみ関数コールされる) */
 void main_task(intptr_t unused) {
+    /* センサー入力ポートの設定 */
+    ev3_sensor_config(touch_sensor ,TOUCH_SENSOR);
+    ev3_sensor_config(color_sensor ,COLOR_SENSOR);
+    ev3_sensor_config(sonar_sensor ,ULTRASONIC_SENSOR);
+    ev3_sensor_config(gyro_sensor  ,GYRO_SENSOR);
     
-    printf("Start MainTask!!\n");
+    /* モーター出力ポートの設定 */
+    ev3_motor_config(arm_motor     ,LARGE_MOTOR);
+    ev3_motor_config(left_motor    ,MEDIUM_MOTOR);
+    ev3_motor_config(right_motor   ,MEDIUM_MOTOR);
 
-    printf("Start Ichi Task!!\n");
+    /* タスク呼び出し */
     sta_cyc(ICHI_CYC);
-
-
     /* タスク終了 */
     ext_tsk();
 }
 
 void ichi_task(intptr_t exinf){
-    float V = ((float)(get_batV())/1000);
-    float C = ((float)(get_batC())/1000);
+    int LP = 50;
+    int RP = 50;
 
-    printf("bat_vol is %f : ", V);
-    printf("bat_crr is %f : ", C);
-    printf("\n");
+    ev3_motor_set_power(left_motor, LP);
+    ev3_motor_set_power(right_motor, RP);
 
 }
