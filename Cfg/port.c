ded2f690a25229a6053e5a1ccffbc448387bad25
Ichic <Ichicoma@outlook.jp>
2024-08-24 23:13:53 +0900

    fix

--- Cfg/port.c
+++ Cfg/port.c
@@ -1,12 +1,13 @@
+#include "ev3api.h"
 #include "port.h"
 
 /* センサーの定義 */
-touch_sensor    = EV3_PORT_1;
-color_sensor    = EV3_PORT_2;
-sonar_sensor    = EV3_PORT_3;
-gyro_sensor     = EV3_PORT_4;
+extern const sensor_port_t touch_sensor = EV3_PORT_1;
+extern const sensor_port_t color_sensor = EV3_PORT_2;
+extern const sensor_port_t sonar_sensor = EV3_PORT_3;
+extern const sensor_port_t gyro_sensor  = EV3_PORT_4;
 
 /* モータの定義 */
-arm_motor       = EV3_PORT_A;
-left_motor      = EV3_PORT_C;
-right_motor     = EV3_PORT_B;
+extern const motor_port_t arm_motor     = EV3_PORT_A;
+extern const motor_port_t left_motor    = EV3_PORT_C;
+extern const motor_port_t right_motor   = EV3_PORT_B;
