e719285e61dd13e4f78d869c823409bf7df2749c
市毛大貴 <d.ichige.cb@hitachi-kenki.com>
2024-08-24 17:23:50 +0900

    fix2

--- Cfg/port.c
+++ Cfg/port.c
@@ -1,6 +1,19 @@
 #include "ev3api.h"
 #include "port.h"
 
+/* センサーポートの定義 */
+extern const sensor_port_t
+    touch_sensor    = EV3_PORT_1,
+    color_sensor    = EV3_PORT_2,
+    sonar_sensor    = EV3_PORT_3,
+    gyro_sensor     = EV3_PORT_4;
+
+/* モーターポートの定義 */
+extern const motor_port_t
+    arm_motor       = EV3_PORT_A,
+    left_motor      = EV3_PORT_C,
+    right_motor     = EV3_PORT_B;
+
 void set_ports(){
     /* センサー入力ポートの設定 */
     ev3_sensor_config(touch_sensor ,TOUCH_SENSOR);
