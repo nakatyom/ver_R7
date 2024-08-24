0ef9c657ebbe13ed904fbf8fb9962e90f0105a47
Ichic <Ichicoma@outlook.jp>
2024-08-24 23:10:15 +0900

    add port.c

---
+++ Cfg/port.c
@@ -0,0 +1,12 @@
+#include "port.h"
+
+/* センサーの定義 */
+touch_sensor    = EV3_PORT_1;
+color_sensor    = EV3_PORT_2;
+sonar_sensor    = EV3_PORT_3;
+gyro_sensor     = EV3_PORT_4;
+
+/* モータの定義 */
+arm_motor       = EV3_PORT_A;
+left_motor      = EV3_PORT_C;
+right_motor     = EV3_PORT_B;
--- Cfg/port.h
+++ Cfg/port.h
@@ -4,16 +4,14 @@
 #include "ev3api.h"
 
 /* センサーポートの定義 */
-extern const sensor_port_t
-    touch_sensor    = EV3_PORT_1,
-    color_sensor    = EV3_PORT_2,
-    sonar_sensor    = EV3_PORT_3,
-    gyro_sensor     = EV3_PORT_4;
+extern const sensor_port_t touch_sensor;
+extern const sensor_port_t color_sensor;
+extern const sensor_port_t sonar_sensor;
+extern const sensor_port_t gyro_sensor;
 
 /* モーターポートの定義 */
-extern const motor_port_t
-    arm_motor       = EV3_PORT_A,
-    left_motor      = EV3_PORT_C,
-    right_motor     = EV3_PORT_B;
+extern const motor_port_t arm_motor;
+extern const motor_port_t left_motor;
+extern const motor_port_t right_motor;
 
 #endif /* _PORT_H */
