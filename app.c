bd931acdcd2e49804b2071be9a3c528e35f16bf0
Ichic <Ichicoma@outlook.jp>
2024-08-24 18:36:00 +0900

    fix

--- app.c
+++ app.c
@@ -39,14 +39,15 @@ void motor_task(intptr_t exinf){
     left_counts = ev3_motor_get_counts(left_motor);
     right_counts = ev3_motor_get_counts(right_motor);
 
-    if(left_counts > 180 || left_counts < 0){
+    if((left_counts > 180 && left_power > 0) || (left_counts < 0 && left_power < 0)){
         left_power *= -1;
     }
 
-    if(right_counts > 180 || right_counts < 0){
+    if((right_counts > 180 && right_power > 0) || (right_counts < 0 && right_power < 0)){
         right_power *= -1;
     }
 
     ev3_motor_set_power(left_motor, left_power);
     ev3_motor_set_power(right_motor, right_power);
+    printf("%d, %d\n",);
 }
