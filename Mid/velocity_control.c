27ab4c5977ccea761efc1ffbd5f41f4e008857da
icepixie071 <sekiyu0701@gmail.com>
2024-08-27 20:54:15 +0900

    ゲイン調整

--- Mid/velocity_control.c
+++ Mid/velocity_control.c
@@ -35,7 +35,7 @@ float mid_PID_str_velo(float tag, float maj){
 
 float mid_PID_rot_velo(float tag, float maj){ 
 
-    const float kp = 1.0f;
+    const float kp = 0.5f;
     const float ki = 0.0f;
     const float kd = 0.0f;
 
