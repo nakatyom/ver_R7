c2da9cf27859ec56ec565ebe3759fc98961c7977
icepixie071 <sekiyu0701@gmail.com>
2024-08-27 22:21:40 +0900

    ゲイン調整

--- Mid/linetrace.c
+++ Mid/linetrace.c
@@ -4,8 +4,8 @@
 
 float mid_PID_line_pos(float tag, float maj){ 
 
-    const float kp = 0.2f;
-    const float ki = 0.03f;
+    const float kp = 0.1f;
+    const float ki = 0.0f;
     const float kd = 0.01f;
 
     static float intg;
