28a8cbb4454be09668d81a788468cd8ed48f875c
NomuraNaoki <nomk8755@icloud.com>
2024-09-09 21:23:08 +0900

    逆回転を正しそうにした

--- Mid/odometry.c
+++ Mid/odometry.c
@@ -39,9 +39,19 @@ void get_crntCoordinate(struct coordinate* crnt_coordinate){
         if(abs(delta_LL) != abs(delta_LR) && abs(delta_LL) > abs(delta_LR)){
             // printf("脳筋ブレイクポイント,左:%f,右:%f\n",delta_LL,delta_LR);
             delta_rad = (abs(delta_LL) - abs(delta_LR)) / (float)wheel_dist;
+            // 逆回転を検知
+            if(delta_LL > 0 && delta_LR < 0){
+                delta_rad = -delta_rad;
+            }
         }else if(abs(delta_LL) != abs(delta_LR) && abs(delta_LL) < abs(delta_LR)){
             delta_rad = (abs(delta_LR) - abs(delta_LL)) / (float)wheel_dist;
+            // 逆回転を検知
+            if(delta_LL > 0 && delta_LR < 0){
+                delta_rad = -delta_rad;
+            }
         }
+                
+        
 
         // 分子が0になる
         else{
@@ -54,10 +64,7 @@ void get_crntCoordinate(struct coordinate* crnt_coordinate){
             }
         }
 
-        // 逆回転を検知
-        if(delta_LL > 0 && delta_LR < 0){
-            delta_rad = -delta_rad;
-        }
+
 
         delta_theta = (delta_rad * 360) / 3.141592;
 
