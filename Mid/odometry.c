9082a7021e8ae442cdcc1fa9c9240e402e8b38fc
市毛大貴 <d.ichige.cb@hitachi-kenki.com>
2024-09-10 21:45:11 +0900

    test odo

--- Mid/odometry.c
+++ Mid/odometry.c
@@ -29,35 +29,30 @@ void get_crntCoordinate(struct coordinate* crnt_coordinate){
     float delta_rad_e = 0.0;
     float delta_theta_e = 0.0;
 
-    if( (delta_LL > 0.0 && delta_LR < 0.0) || (delta_LL < 0.0 && delta_LR > 0.0) ){ //旋回している
+    if(delta_LL > 0.0 && delta_LR < 0.0){ //時計回りに旋回している
         //ロボットの移動距離
         delta_L = 0.0;
-
         // 旋回量の計算
-        // 分子が0にならない
-        if(abs(delta_LL) > abs(delta_LR)){
-            delta_rad_e = (abs(delta_LL) + abs(delta_LR)) / (float)wheel_dist;
-            if(delta_LL < 0.0f){
-                delta_rad_e = -delta_rad_e;
-            }
-        }
-        else if(abs(delta_LL) < abs(delta_LR)){
-            delta_rad_e = (abs(delta_LR) + abs(delta_LL)) / (float)wheel_dist;
-            delta_rad_e = -delta_rad_e;
-        }
-        else{// 分子が0になる（シミュレータ上の措置）
-            delta_rad_e = delta_LL / (float)wheel_dist;                    
-        }
+        delta_rad_e = (abs(delta_LL) + abs(delta_LR)) / (float)wheel_dist;
+
+    }
+    else if((delta_LL < 0.0 && delta_LR > 0.0)){ // 反時計回りに旋回している
+        //ロボットの移動距離
+        delta_L = 0.0;
+        // 旋回量の計算
+        delta_rad_e = -1 * (abs(delta_LL) + abs(delta_LR)) / (float)wheel_dist;
+
     }
     else { //直進・曲進している
         //ロボットの移動距離
         delta_L = (delta_LL + delta_LR)/(float)2.0;
         //ロボットの旋回量
         delta_rad_e = (delta_LL - delta_LR) / (float)wheel_dist;
-
+        
         if(abs(delta_rad_e) > 0.174){ //delta_rad_eが十分大きい
             delta_L = 2 * (delta_L / delta_rad_e) * sin(delta_rad_e / 2);
         }
+        
     }
     delta_theta_e = (float)((delta_rad_e * 180.0) / PI_FLOAT);
 
@@ -67,8 +62,8 @@ void get_crntCoordinate(struct coordinate* crnt_coordinate){
 
     // 現在座標を計算する
     float pre_rad = PI_FLOAT * pre_coordinate.theta / 180.0;
-    float delta_rad = delta_rad_e;      // 旋回量計算に使用するデバイスの選択
-    float delta_theta = delta_theta_e;  // 旋回量計算に使用するデバイスの選択
+    float delta_rad = delta_rad_g;      // 旋回量計算に使用するデバイスの選択
+    float delta_theta = delta_theta_g;  // 旋回量計算に使用するデバイスの選択
 
     crnt_coordinate->x      = pre_coordinate.x + (float)((double)delta_L * cos(pre_rad + (delta_rad / 2.0)));
     crnt_coordinate->y      = pre_coordinate.y + (float)((double)delta_L * sin(pre_rad + (delta_rad / 2.0)));
