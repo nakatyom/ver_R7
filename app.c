6542859ea9df3a92c1e0eaa610602ad11cc240a6
市毛大貴 <d.ichige.cb@hitachi-kenki.com>
2024-09-10 21:10:01 +0900

    update rotate degree 720->180

--- app.c
+++ app.c
@@ -36,11 +36,11 @@ void boss_task(intptr_t exinf){
         get_crntCoordinate(&crnt);
         printf("x=%f, y=%f, theta=%f\n",crnt.x, crnt.y,crnt.theta);
 
-        if(crnt.theta < 720){
+        if(crnt.theta <= 180){
             motor_set_power(left_motor,80);
             motor_set_power(right_motor,-80);
         }
-        else if(crnt.theta > 720){
+        else if(crnt.theta > 180){
             motor_set_power(left_motor,0);
             motor_set_power(right_motor,0);
         }
