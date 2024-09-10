851feda5b9231e88b7159f7a25e17786cb4da1f5
NomuraNaoki <nomk8755@icloud.com>
2024-09-10 13:49:51 +0900

    crnt.theta表示

--- app.c
+++ app.c
@@ -35,7 +35,7 @@ void boss_task(intptr_t exinf){
     else if(test == 1){
         get_crntCoordinate(&crnt);
         //printf("x=%f , y=%f , θ=%f \n", crnt.x , crnt.y , crnt.theta);
-        printf("ond of line\n");
+        printf("角度:%f end of line\n",crnt.theta);
         //printf("x=%f, y=%f, theta=%f\n",crnt.x, crnt.y,crnt.theta);
     }
 
