ba6fa517b7705a1dcc4dbba1c40ee40f53042980
NomuraNaoki <nomk8755@icloud.com>
2024-09-10 13:30:58 +0900

    app.c変更デバッグ用

--- app.c
+++ app.c
@@ -34,7 +34,8 @@ void boss_task(intptr_t exinf){
     
     else if(test == 1){
         get_crntCoordinate(&crnt);
-        printf("x=%f , y=%f , θ=%f \n", crnt.x , crnt.y , crnt.theta);
+        //printf("x=%f , y=%f , θ=%f \n", crnt.x , crnt.y , crnt.theta);
+        printf("ond of line\n")
         //printf("x=%f, y=%f, theta=%f\n",crnt.x, crnt.y,crnt.theta);
     }
 
