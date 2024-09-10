b7a34ce8b7fa085b2a52d622a3b9a07b621b2ed7
市毛大貴 <d.ichige.cb@hitachi-kenki.com>
2024-09-11 08:37:18 +0900

    fix color.c

--- Drv/color.h
+++ Drv/color.h
@@ -5,7 +5,7 @@
 
 /* extern functions */
 extern uint8_t color_sensor_get_reflect(sensor_port_t port);
-extern void    color_sensor_get_rgb_raw(sensor_port_t port, rgb_raw_t rgb_val);
+extern void    color_sensor_get_rgb_raw(sensor_port_t port, rgb_raw_t* rgb_val);
 
 /* using_sensor_task */
 extern void color_sensor_update(sensor_port_t port);
