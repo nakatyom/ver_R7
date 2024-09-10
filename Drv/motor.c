4cd859e7745bd8b13d8614c842e80d909163e4be
NomuraNaoki <nomk8755@icloud.com>
2024-09-10 18:35:15 +0900

    試しに0.1s周期

--- Drv/motor.c
+++ Drv/motor.c
@@ -52,7 +52,6 @@ extern int32_t motor_get_counts(motor_port_t port){
     for(int i=0; i<1; i++){
         crnt_enc[port] = ev3_motor_get_counts(port);
         // i += checkMissingData(port);
-        printf("a");
     }
 
 
