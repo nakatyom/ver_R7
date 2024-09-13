269cadcc41e7df6802fb20815686ba009577fd01
nakatyom <k.nakano.wz@hotachi-kenki.com>
2024-09-13 13:54:37 +0900

    解析１　color 呼び出し前

--- Apl/linetrace.c
+++ Apl/linetrace.c
@@ -50,7 +50,7 @@ struct coordinate crnt_line = {0.0, 0.0, 0.0};//自己位置座標
 
 extern bool_t judge_blue(){
     int reflection = color_sensor_get_reflect(color_sensor);
-    color_sensor_get_rgb_raw(color_sensor,&crnt_rgb_line);
+    //color_sensor_get_rgb_raw(color_sensor,&crnt_rgb_line);
     printf("ref = %d | r = %d | g = %d | b = %d\n",reflection, crnt_rgb_line.r, crnt_rgb_line.g, crnt_rgb_line.b);
     if(( crnt_rgb_line.r >=80 && crnt_rgb_line.r <=90 ) && ( crnt_rgb_line.g >=110 && crnt_rgb_line.g <=225 ) && ( crnt_rgb_line.b >=140 && crnt_rgb_line.b <=150 )&& ( reflection >=85 && reflection <=150 )  ){
         printf("_____________________BLUE JUDGE_____________________\n");
@@ -64,7 +64,7 @@ extern bool_t judge_blue(){
 
 extern bool_t judge_black(){
     int reflection = color_sensor_get_reflect(color_sensor);
-    color_sensor_get_rgb_raw(color_sensor,&crnt_rgb_line);
+    //color_sensor_get_rgb_raw(color_sensor,&crnt_rgb_line);
     printf("ref = %d | r = %d | g = %d | b = %d\n",reflection, crnt_rgb_line.r, crnt_rgb_line.g, crnt_rgb_line.b);
     if(( crnt_rgb_line.r >=70 && crnt_rgb_line.r <=80 ) && ( crnt_rgb_line.g >=80 && crnt_rgb_line.g <=90 ) && ( crnt_rgb_line.b >=95 && crnt_rgb_line.b <=100 )&& ( reflection >=68 && reflection <=77 )  ){
         printf("_____________________BLACK JUDGE_____________________\n");
@@ -79,7 +79,7 @@ int linetrace(void){
     get_crntCoordinate(&crnt_line);
     printf("x=%f, y=%f, theta=%f | ",crnt_line.x, crnt_line.y, crnt_line.theta);
     // BLUE検知まで走行する処理。
-    int reflection = color_sensor_get_reflect(color_sensor);
+    int reflection = ev3_color_sensor_get_reflect(color_sensor);
     float velo_rot_target;
     
     if (divion == 0){
