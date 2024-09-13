99f882cd7cf765732942e43fe46cd6a4aff0b304
市毛大貴 <d.ichige.cb@hitachi-kenki.com>
2024-09-13 22:09:26 +0900

    add vfb

---
+++ Mid/velocity_control.h
@@ -0,0 +1,11 @@
+#ifndef _VELOCITY_CONTROL_H
+#define _VELOCITY_CONTROL_H
+ 
+#include "ev3api.h"
+#include <stdio.h>
+#include "app.h"
+#include "port.h"
+ 
+extern void mid_velocity_control(float, float);
+ 
+#endif	// _VELOCITY_CONTROL_H
