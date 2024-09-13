b782cac326fb64916abaabbfc73c27a1db0e22cc
NomuraNaoki <nomk8755@icloud.com>
2024-09-13 11:48:38 +0900

    コメントアウトを修正

--- Mid/odometry.h
+++ Mid/odometry.h
@@ -8,4 +8,4 @@
 extern void get_crntCoordinate(struct coordinate* crnt_coordinate);
 extern void get_preCoordinate(struct coordinate* pre_coordinate);
  
-#endif /* _ODOMETRY_H *
+#endif /* _ODOMETRY_H */
