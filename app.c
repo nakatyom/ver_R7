6840957d7a15e9b40bd861d47c7b8db82c5caacb
市毛大貴 <d.ichige.cb@hitachi-kenki.com>
2024-09-13 21:53:51 +0900

    fix

--- app.c
+++ app.c
@@ -63,7 +63,7 @@ void sens_task(intptr_t exinf){
     printf("ref = %d | r = %d | g = %d | b = %d\n",ref, rgb.r, rgb.g, rgb.b);
     */
 
-   mid_velocity_control(500.0f, 0.0f);
+   mid_velocity_control(50.0f, 50.0f);
 
    //gb_raw_t crnt_rgb_app = {0.0, 0.0, 0.0};
 
