5f1b685f0897fd2be1335a6ff238b4f556547744
NomuraNaoki <nomk8755@icloud.com>
2024-09-13 10:45:45 +0900

    Body情報を修正。

--- Cfg/body.c
+++ Cfg/body.c
@@ -1,6 +1,11 @@
 #include "body.h"
 
+/* SIM
 const int wheel_size  = 90; // タイヤ直径[mm]
 const int wheel_dist  = 140; // タイヤ間の距離[mm] 
+*/
+/*REAL*/
+const int wheel_size  = 94; // タイヤ直径[mm]
+const int wheel_dist  = 126; // タイヤ間の距離[mm] 
 const int color_dist  = 45.5; // タイヤ間中点からカラーセンサまでの距離[mm]
 const int sonic_hight = 100; // ソニックセンサ高さ
