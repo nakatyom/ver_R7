7058b781a7a0fd931dc7e09657f08221f93020f8
市毛大貴 <d.ichige.cb@hitachi-kenki.com>
2024-09-13 18:47:22 +0900

    fix

--- app.c
+++ app.c
@@ -47,7 +47,8 @@ void boss_task(intptr_t exinf){
     }
 
     float tgtV = get_TgtVelcity((float)(cunt*0.1), 11, time, velo);
-    printf("TgtV: %f\n",tgtV);
+    printf("crnt_time: %f ", (float)(cunt*0.1));
+    printf("TgtV: %f\n", tgtV);
 
     mid_velocity_control(tgtV, 0.0);    
 
