917d9882dea3895b93c7acef64b0420173a25a81
nakatyom <k.nakano.wz@hotachi-kenki.com>
2024-09-13 23:04:57 +0900

    verocity check

--- Apl/linetrace.c
+++ Apl/linetrace.c
@@ -98,7 +98,8 @@ extern int linetrace(){
         if((crnt_line.x >=2750 && crnt_line.y >= -300) || (crnt_line.x >=2750 && crnt_line.y <= -1800)){
             printf("判定1 \n");
             velo_rot_target = mid_PID_line_pos(80.0f, (float)reflection,50);
-            mid_velocity_control(50.0f, -velo_rot_target);
+            //mid_velocity_control(50.0f, -velo_rot_target);
+            mid_velocity_control(50.0f, 0.0f);
             if (crnt_line.x >=2750 && crnt_line.y <= -1800 && kotesaki==false){
                 kotesaki=true;
             }
