0932e4ae8c7cbc7bb6ad20d01de64dc7b65c1ddc
NomuraNaoki <nomk8755@icloud.com>
2024-09-14 16:42:04 +0900

    add VFB in gyak

--- Mid/gyak_min.c
+++ Mid/gyak_min.c
@@ -2,6 +2,8 @@
 #include <math.h>
 
 #include "common.h"
+#include "port.h"
+#include "body.h"
 #include "gyak_min.h"
 
 /* static functions */
@@ -86,4 +88,108 @@ float get_TgtRate(float crnt_time, int div_num, float* div_time, float* tgt_rate
     }
 
     return tgt_rate[cunt_r];
+}
+
+
+/* 関VFB移植 */ 
+float mid_PID_str_velo(float tag, float maj){
+ 
+    const float kp = 0.15f;
+    const float ki = 0.02f;
+    const float kd = 0.0f;
+ 
+    static float intg;
+    static float err_pre;
+    static float err;
+    err_pre = err;
+    err = tag - maj;
+    intg += err;
+ 
+    if (intg > 2000.0f)    intg = 2000.0f;
+    if (intg < -2000.0f)   intg = -2000.0f;
+ 
+    return ((err * kp) + (intg * ki) + ((err - err_pre) * kd));
+}
+ 
+ 
+float mid_PID_rot_velo(float tag, float maj){
+ 
+    const float kp = 0.4f;
+    const float ki = 0.1f;
+    const float kd = 0.05f;
+ 
+    static float intg;
+    static float err_pre;
+    static float err;
+    err_pre = err;
+    err = tag - maj;
+    intg += err;
+ 
+    if (intg > 1000.0f)    intg = 1000.0f;
+    if (intg < -1000.0f)   intg = -1000.0f;
+ 
+    return ((err * kp) + (intg * ki) + ((err - err_pre) * kd));
+}
+ 
+ 
+float mid_LPF_str_velo(float maj){
+ 
+    const float k = 0.85f;
+ 
+    static float out;
+ 
+    out = (out * k) + (maj * (1.0f - k));
+ 
+    return out;
+}
+ 
+void mid_velocity_control(float velo_str_tgt, float velo_rot_tgt){
+    float enc_corr_r = 1.0;
+    float enc_corr_l = 1.0;
+    float sample_rate = 50.0;
+    
+ 
+    static int32_t cnt_r;
+    static int32_t cnt_l;
+    static int32_t cnt_r_pre;
+    static int32_t cnt_l_pre;
+ 
+    /* 並進速度計算 */
+    cnt_r_pre = cnt_r;
+    cnt_l_pre = cnt_l;
+    cnt_r = motor_get_counts(right_motor); 
+    cnt_l = motor_get_counts(left_motor);
+ 
+    float velo_r_tmp  = (((float)(cnt_r - cnt_r_pre) * enc_corr_r) * sample_rate); // cnt ⇒ deg/sec(motor)
+    float velo_l_tmp  = (((float)(cnt_l - cnt_l_pre) * enc_corr_l) * sample_rate); // cnt ⇒ deg/sec(motor)             
+    float velo_r    = (velo_r_tmp / 360.0f) * (PI_FLOAT * wheel_size);   // deg/sec(motor) ⇒ mm/sec(motor) 
+    float velo_l    = (velo_l_tmp / 360.0f) * (PI_FLOAT * wheel_dist);   // deg/sec(motor) ⇒ mm/sec(motor)
+    float velo_str  = mid_LPF_str_velo( ((velo_r + velo_l) * 0.5f)); // mm/sec(robot)
+ 
+    /* 旋回速度計算 */ 
+    float velo_rot = (float)ev3_gyro_sensor_get_rate(gyro_sensor);   // deg/sec (yaw rate)
+ 
+    /* 速度操作量計算（FB項算出）*/
+    // float velo_str_u = mid_PID_str_velo(velo_str_tgt, velo_str);    // mm/sec(robot)
+    float velo_rot_u = mid_PID_rot_velo(velo_rot_tgt, velo_rot);    // deg/sec(yaw rate)
+ 
+    /* 速度操作量計算（FF項とFB項の合成）*/
+    float mot_r_u_str = (velo_str_tgt / (PI_FLOAT * wheel_size)) * 360.0f;  // mm/sec(robot) ⇒ deg/sec(motor)
+    float mot_l_u_str = (velo_str_tgt / (PI_FLOAT * wheel_size)) * 360.0f;  // mm/sec(robot) ⇒ deg/sec(motor)
+    float mot_r_u_rot = (wheel_dist / wheel_size) * (velo_rot_tgt + velo_rot_u);            // deg/sec(yaw rate) ⇒ deg/sec(motor)
+    float mot_l_u_rot = (wheel_dist / wheel_size) * (velo_rot_tgt + velo_rot_u);            // deg/sec(yaw rate) ⇒ deg/sec(motor)
+    /* 速度操作量計算（並進速度と旋回速度の合成）*/
+    float mot_r_u_tmp = mot_r_u_str + mot_r_u_rot;  // deg/sec(motor)
+    float mot_l_u_tmp = mot_l_u_str - mot_l_u_rot;  // deg/sec(motor)
+    /* 操作量計算 */
+    int mot_r_u = (int)(mot_r_u_tmp * 1.0f);
+    int mot_l_u = (int)(mot_l_u_tmp * 1.0f);
+ 
+    if (mot_r_u >  100)  mot_r_u = 100;
+    if (mot_r_u < -100)  mot_r_u = -100;
+    if (mot_l_u >  100)  mot_l_u = 100;
+    if (mot_l_u < -100)  mot_l_u = -100;
+ 
+    ev3_motor_set_power(right_motor, mot_r_u);
+    ev3_motor_set_power(left_motor, mot_l_u);
 }
