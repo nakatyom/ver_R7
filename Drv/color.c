0ad86334d649169fdfcdb4754c5a9f45839650c7
NomuraNaoki <nomk8755@icloud.com>
2024-09-15 13:34:38 +0900

    rm refapi

--- Drv/color.c
+++ Drv/color.c
@@ -12,18 +12,6 @@
 
 
 /* extern functions */
-uint8_t color_sensor_get_reflect(sensor_port_t port){
-    if(port != color_sensor){
-        printf("An invalid value entered in color_sensor_get_reflect().\n");
-        return 0;
-    }
-
-    /* 現在値の取得 */
-    crnt_ref = ev3_color_sensor_get_reflect(port);
-
-    return crnt_ref;
-}
-
 void color_sensor_get_rgb_raw(sensor_port_t port, rgb_raw_t* rgb_val){
     if(port != color_sensor){
         printf("An invalid value entered in color_sensor_get_rgb_raw().\n");
@@ -41,15 +29,3 @@ void color_sensor_get_rgb_raw(sensor_port_t port, rgb_raw_t* rgb_val){
 
 
 /* using_sensor_task */
-void color_sensor_update(sensor_port_t port){
-    if(port != color_sensor){
-        printf("An invalid value entered in color_sensor_update().\n");
-        return;
-    }
-
-    /* 現在値の取得 */
-    crnt_ref = ev3_color_sensor_get_reflect(port);
-    ev3_color_sensor_get_rgb_raw(port, &crnt_rgb);
-
-    return;
-}
--- Drv/color.h
+++ Drv/color.h
@@ -4,10 +4,6 @@
 #include "ev3api.h"
 
 /* extern functions */
-extern uint8_t color_sensor_get_reflect(sensor_port_t port);
 extern void    color_sensor_get_rgb_raw(sensor_port_t port, rgb_raw_t* rgb_val);
-
-/* using_sensor_task */
-extern void color_sensor_update(sensor_port_t port);
 
 #endif /* _COLOR_H */
